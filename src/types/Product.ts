ae928032a7bbd7c71c8157e2af1d4bc8317782d6
brunomaatias <bruno.gabriel0589@gmail.com>
2026-02-26 13:11:12 -0300

    feat: creation of the type ProductionSuggestion on Product.ts.

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,5 +1,14 @@
 export interface Product {
+    productId?: number;
     code: string;
     name: string;
     price: number;
+}
+
+export interface ProductionSuggestion {
+    productId: number; 
+    productName: string;
+    quantityPossible: number;
+    unitPrice: number;
+    totalValue: number;
 }
