51d26b80c2092e983aa44d1cabb710d1352b596d
brunomaatias <bruno.gabriel0589@gmail.com>
2026-02-27 13:22:13 -0300

    feat: creation of methods to get, delete and update ProductRawMaterials.

--- src/services/productRawMaterialService.tsx
+++ src/services/productRawMaterialService.tsx
@@ -10,3 +10,18 @@ export const postProductRawMaterials = async (product: ProductRawMaterial): Prom
   const response = await api.post<ProductRawMaterial>("/product-raw-materials", product );
   return response.data; 
 };
+
+export const getProductRawMateralsByIdProduct = async (productId: number): Promise<ProductRawMaterial[]> => {
+  const response = await api.get<ProductRawMaterial[]>(`/product-raw-materials/product/${productId}`);
+  return response.data;
+};
+
+export const deleteProductRawMaterialsById = async (productRawMaterialId: number): Promise<ProductRawMaterial> => {
+  const response = await api.delete<ProductRawMaterial>(`/product-raw-materials/${productRawMaterialId}`);
+  return response.data;
+};
+
+export const updateProductRawMaterials = async (productRawMaterial: ProductRawMaterial): Promise<ProductRawMaterial> => {
+  const response = await api.put<ProductRawMaterial>(`/product-raw-materials/${productRawMaterial.productRawMaterialId}`, productRawMaterial);
+  return response.data;
+};
--- src/types/ProductRawMaterial.ts
+++ src/types/ProductRawMaterial.ts
@@ -1,4 +1,5 @@
 export interface ProductRawMaterial {
+    productRawMaterialId?: number;
     rawMaterialId: number;
     productId?: number;
     requiredQuantity: number;
